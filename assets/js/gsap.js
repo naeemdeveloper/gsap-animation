15e2ea53aa7d022a106f65a61aa7ffea1253fc11
naeemtechlyn <naeem.techlyn@gmail.com>
2024-07-03 17:45:23 +0600

    git push from naeemtechlyn

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,10 +1,9 @@
-
 var tl = gsap.timeline()
 
 tl.from('.header-area .logo-area', {
     y: -50,
     duration: 1,
-    delay: .5,
+    delay: .3,
     opacity: 0, 
     ease: 'bounce',
 })
@@ -13,7 +12,7 @@ tl.from('.header-area h2', {
     y: 20,
     duration: 1,
     opacity: 0, 
-    scale: .4,
+    scale: .8,
 })
 
 tl.from('.header-area form', {
@@ -30,17 +29,18 @@ gsap.from('.image-area', {
     scrollTrigger: {
         trigger: '.image-area',
         scroller: 'body',
-        scrub: 1,
+        scrub: 2,
+        start: 'top 0%'
     }
 })
 
 gsap.to('.big-title span', {
-    x: "-60%",
+    x: "-110%",
     scrollTrigger: {
         trigger: '.big-title',
         scroller: 'body',
         start: 'top 0%',
-        end: 'top -150%',
+        end: 'top -100%',
         scrub: 2,
         pin: true,
     }
