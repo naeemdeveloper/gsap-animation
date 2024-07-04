22bfb65d8930994be54fda394d12575020f1f5d0
naeemtechlyn <naeem.techlyn@gmail.com>
2024-07-04 18:41:45 +0600

    push from naeemtechlyn

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,47 +1,88 @@
+
 var tl = gsap.timeline()
 
 tl.from('.header-area .logo-area', {
     y: -50,
-    duration: 1,
+    duration: .7,
     delay: .3,
     opacity: 0, 
     ease: 'bounce',
 })
 
 tl.from('.header-area h2', {
-    y: 20,
-    duration: 1,
+    duration: .7,
     opacity: 0, 
-    scale: .8,
+    scale: .9,
+    ease: 'power3',
 })
 
 tl.from('.header-area form', {
-    y: 30,
-    duration: 1,
+    duration: .7,
     opacity: 0, 
     ease: 'power2',
 })
 
 gsap.from('.image-area', {
-    scaleX: .5,
-    duration: 1,
-    delay: .5,
+    scaleX: .6,
+    duration: .7,
+    delay: .3,
     scrollTrigger: {
         trigger: '.image-area',
         scroller: 'body',
         scrub: 2,
-        start: 'top 0%'
+        start: 'top 0%',
+        end: 'top: -100%'
     }
 })
 
 gsap.to('.big-title span', {
-    x: "-110%",
+    x: "-70%",
     scrollTrigger: {
         trigger: '.big-title',
         scroller: 'body',
         start: 'top 0%',
         end: 'top -100%',
-        scrub: 2,
+        scrub: 6,
         pin: true,
     }
 });
+
+var cursorArea = document.querySelector('.cursor-area');
+var cursor = document.querySelector('.custom-cursor');
+
+window.addEventListener('mousemove', function(dets){
+    gsap.to(cursor, {
+        x: dets.x,
+        y: dets.y,
+        duration: .7,
+        ease: 'back.out',
+    })
+}) 
+
+
+var navbarOpen = document.querySelector('.navbar-open');
+var navbarClose = document.querySelector('.navbar-close');
+
+var menuArea = gsap.timeline()
+
+menuArea.to('.menu-area', {
+    right: 0,
+    duration: .5,
+})
+
+menuArea.from('.menu-area li', {
+    x: 70,
+    duration: .5,
+    stagger: .2,
+    opacity: 0,
+})
+
+menuArea.pause()
+
+navbarOpen.addEventListener('click', function(){
+    menuArea.play()
+})
+
+navbarClose.addEventListener('click', function(){
+    menuArea.reverse()
+})
